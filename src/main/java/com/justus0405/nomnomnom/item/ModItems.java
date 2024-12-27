827c92a1a05ce772ebb3b35a80bc56212bc15556
Justus0405 <justus040506@gmail.com>
2024-12-27 22:01:05 +0100

    Removed unused imports

--- src/main/java/com/justus0405/nomnomnom/item/ModItems.java
+++ src/main/java/com/justus0405/nomnomnom/item/ModItems.java
@@ -3,8 +3,6 @@ package com.justus0405.nomnomnom.item;
 import com.justus0405.nomnomnom.NomNomNom;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -134,8 +132,7 @@ public class ModItems {
     public static final RegistryObject<Item> TEA = ITEMS.register("tea",
             () -> new CustomMilkItem(new Item.Properties().tab(ModItemGroup.NOMNOMNOM_GROUP)));
 
-
-
+    //EventBus Registration
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
